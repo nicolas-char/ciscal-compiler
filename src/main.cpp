c7749689844442cbeacebe664f4a8028e112e38e
nicolas-char <n.charalampopoulos01@gmail.com>
2026-04-08 22:16:03 +0300

    feat: implement recursive descent parser for variable declarations

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,81 @@
 #include <iostream>
 #include "tokens.h"
 
+extern int yylineno;
+
 using namespace std;
 
     /* A safe copy of ours that isn't subject to be spoiled by Flex's internal processes */
 string tokenValue;
 
-int main() {
-    int token;
+    /* Holds the Token's tpye (ID produced by enum). 
+    `tokenValue is what holds the lexema` */
+int currentToken;
 
-    cout << "Ciscal Lexer started...\n";
-    cout << "Type something and press Enter (or Ctrl+D to exit):\n";
-    
-    /* Start the lexical analysis */
-    while ((token = yylex()) != 0) {
-        cout << "Token ID: " << token <<"\n";
+void nextToken() {
+    currentToken = yylex();
+}
 
-        if (token == TOKEN_IDENTIFIER || token == TOKEN_NUMBER || token == TOKEN_UNKNOWN)
-            cout << " (Value: " << tokenValue << ")";
-        cout << "\n";
+void match(int expectedToken) {
+    if (currentToken == expectedToken)
+        nextToken();
+    else {
+        cout << "Syntax Error: Expected token " << expectedToken
+            << " but found " << currentToken << " at line " << yylineno << "\n";
+            exit(1);
     }
+}
+
+void parseSequence() {
+    cout << " [Dummy Sequence] ";
+}
+
+void parseSubprograms() {
+    cout << " [Dummy Subprograms] ";
+}
+
+void parseVarList() {    /* Done */
+    if (currentToken == TOKEN_IDENTIFIER) {
+        match(TOKEN_IDENTIFIER);
+        while (currentToken == TOKEN_COMMA) {
+            match(TOKEN_COMMA);
+            match(TOKEN_IDENTIFIER);
+        }
+    }
+}
+
+void parseDeclarations() {   /* Done */
+    if (currentToken == TOKEN_DECLARE) {
+        match(TOKEN_DECLARE);
+        parseVarList();
+        match(TOKEN_ENDDECLARE);
+    }
+}
+
+void parseBlock() {
+    match(TOKEN_LBRACE);
+    parseDeclarations();
+    parseSubprograms();
+    parseSequence();
+    match(TOKEN_RBRACE);
+}
+
+void parseProgram() {
+    match(TOKEN_PROGRAM);
+    match(TOKEN_IDENTIFIER);
+    parseBlock();
+}
+
+int main() {
+    cout << "Ciscal Compiler started...\n";
+
+    nextToken();
+    parseProgram();
+    
+    if (currentToken == TOKEN_EOF)
+        cout << "Parsing completed successfully!\n";
+    else
+        cout << "Syntax Error: Unexpected tokens at the end of file.\n";
     
     return 0;
 }
